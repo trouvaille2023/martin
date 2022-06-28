26f710caa5e866cd8d0503fec2de2f430b51b7cf
听风 <martinwang@ibtool.cn>
2022-06-28 16:57:36 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 
 
 git add deploy
+
 git push origin --delete gh-pages
+
 git branch -d gh-pages
+
 git checkout -b gh-pages
+
 git subtree push --prefix deploy  origin gh-pages
+
+git checkout main
