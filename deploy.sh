e48aea0d4d2d99a5af2c00c280f6ec971eff2c69
Martin <martinwang@ibtool.cn>
2022-06-30 14:06:26 +0800

    :monocle_face: HomeBall

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,18 @@ git checkout -b gh-pages
 git subtree push --prefix deploy  origin gh-pages
 
 git checkout main
+
+git subtree add --prefix deploy gh-pages
+
+git subtree add --prefix  deploy origin main --squash
+
+git subtree pull --prefix deploy origin main --squash
+
+git subtree add --prefix=sub/libpng libpng master --squash
+
+#git subtree add   --prefix=<prefix> <commit>
+#git subtree add   --prefix=<prefix> <repository> <ref>
+#git subtree pull  --prefix=<prefix> <repository> <ref>
+#git subtree push  --prefix=<prefix> <repository> <ref>
+#git subtree merge --prefix=<prefix> <commit>
+#git subtree split --prefix=<prefix> [OPTIONS] [<commit>]
